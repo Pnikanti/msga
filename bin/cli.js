1c9b1164b325587859b8222c6d7bcc3d95afbbb7
Pnikanti <patrick.nikanti@gmail.com>
2025-10-25 23:02:00 +0300

    Implement cli.js. Tidy up.

---
+++ bin/cli.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+import { execSync } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const cwd = process.cwd(); // user project root
+
+// ensure dist exists in the user project
+const distDir = path.join(cwd, "dist");
+
+if (!fs.existsSync(distDir))
+	fs.mkdirSync(distDir, { recursive: true });
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const bin = (name) => path.join(__dirname, "../node_modules/.bin", name);
+
+const libSrc = path.join(__dirname, "../src/msga");
+const userSrc = path.join(cwd, "src");
+
+const msgaLib = `${bin("cpx")} "${path.join(libSrc, "**/*.js")}" ${path.join(distDir, "src/msga")} --watch`;
+const tailwindLib = `${bin("tailwindcss")} -i ${path.join(libSrc, "/msga.css")} -o ${path.join(`${distDir}/src`, "/msga/msga.css")} --watch`;
+const swcApp = `${bin("swc")} ${userSrc} -d ${distDir} --watch`;
+const cpxApp = `${bin("cpx")} "${path.join(userSrc, "*.{html,css}")}" ${distDir}/src --watch`;
+
+// Vite (if you want dev server for app)
+const viteCmd = `${bin("vite")} --config ${path.join(cwd, "vite.config.js")}`;
+
+const concurrently = bin("concurrently");
+
+// parse CLI command
+const command = process.argv[2] || "dev";
+
+switch (command) {
+	case "dev":
+		execSync(`${concurrently} -n tailwind,swc,static,library,vite \
+			-c "magenta,blue,green,yellow,cyan" \
+			"${tailwindLib}" "${swcApp}" "${cpxApp}" "${msgaLib}" "${viteCmd}"`, { stdio: "inherit" }
+		);
+
+		break;
+	default:
+		console.log(`
+      Usage:
+        msga dev     Start dev server (watch mode)
+        msga build   Build project once # NOT IMPLEMENTED
+        msga serve   Preview built site # NOT IMPLEMENTED
+    `);
+}
