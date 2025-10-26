ab22a70a3353c63d3bf9bcb5dfcf8a6e41016ddc
Pnikanti <patrick.nikanti@gmail.com>
2025-10-26 17:36:04 +0200

    cp assets folder as well. use default tailwind.config.js

--- bin/cli.js
+++ bin/cli.js
@@ -19,23 +19,26 @@ const bin = (name) => `npx ${name}`;
 const libSrc = path.join(__dirname, "../src/msga");
 const userSrc = path.join(cwd, "src");
 
-const msgaLib = `${bin("cpx")} "${path.join(libSrc, "**/*.js")}" ${path.join(distDir, "src/msga")} --watch`;
-const tailwindLib = `${bin("tailwindcss")} -i ${path.join(libSrc, "/msga.css")} -o ${path.join(`${distDir}/src`, "/msga/msga.css")} --watch`;
+const concurrently = bin("concurrently");
 
-const swcApp = `${bin("swc")} "${path.join(userSrc, "*.jsx")}" "${path.join(userSrc, "**/*.jsx")}" -d "${distDir}" --config-file "${path.join(__dirname,'../.swcrc')}" --watch`;
-const cpxApp = `${bin("cpx")} "${path.join(userSrc, "*.{html,css}")}" ${distDir}/src --watch`;
+const msgaLib = `${bin("cpx")} "${path.join(libSrc, "**/*.js")}" ${path.join(distDir, "src/msga")} --watch`;
+const tailwindLib = `${bin("tailwindcss")} -i ${path.join(libSrc, "msga.css")} -o ${path.join(distDir, "src/msga/msga.css")} --config "${path.join(__dirname, '../tailwind.config.js')}" --watch`;
+
+const swcApp = `${bin("swc")} "${path.join(userSrc, "*.jsx")}" "${path.join(userSrc, "**/*.jsx")}" -d "${distDir}" --config-file "${path.join(__dirname,'../.swcrc')}" --watch`;c
+
+const cpxHtmlCss = `${bin("cpx")} "${path.join(userSrc, "*.{html,css}")}" "${distDir}/src" --watch`;
+const cpxAssets = `${bin("cpx")} "${path.join(userSrc, "assets/**/*")}" "${distDir}/src/assets" --watch`;
 
 const viteCmd = `${bin("vite")} --config ${path.join(__dirname,'../vite.config.js')}`;
-const concurrently = bin("concurrently");
 
 // parse CLI command
 const command = process.argv[2] || "dev";
 
 switch (command) {
 	case "dev":
-		execSync(`${concurrently} -n tailwind,swc,static,library,vite \
-			-c "magenta,blue,green,yellow,cyan" \
-			"${tailwindLib}" "${swcApp}" "${cpxApp}" "${msgaLib}" "${viteCmd}"`, { stdio: "inherit" }
+		execSync(`${concurrently} -n tailwind,swc,html,assets,library,vite \
+			-c "magenta,blue,green,orange,yellow,cyan" \
+			"${tailwindLib}" "${swcApp}" "${cpxHtmlCss}" "${cpxAssets}" "${msgaLib}" "${viteCmd}"`, { stdio: "inherit" }
 		);
 
 		break;
@@ -43,7 +46,5 @@ switch (command) {
 		console.log(`
       Usage:
         msga dev     Start dev server (watch mode)
-        msga build   Build project once # NOT IMPLEMENTED
-        msga serve   Preview built site # NOT IMPLEMENTED
     `);
 }
